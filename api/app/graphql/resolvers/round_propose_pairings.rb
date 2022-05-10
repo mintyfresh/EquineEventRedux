14585535767cf650c3ee5224bd2f0e84b6cae1ab
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-09 20:30:57 -0400

    Add argument to exclude players from generated pairings.

--- api/app/graphql/resolvers/round_propose_pairings.rb
+++ api/app/graphql/resolvers/round_propose_pairings.rb
@@ -2,15 +2,20 @@
 
 module Resolvers
   class RoundProposePairings < BaseResolver
+    description 'Generates a possible list of pairings for a round.'
+
     type [[Types::PlayerType, { null: true }]], null: false
 
     argument :round_id, ID, required: true
+    argument :exclude_player_ids, [ID], required: false, default_value: [] do
+      description 'Players to be excluded from pairing'
+    end
 
-    def resolve(round_id:)
+    def resolve(round_id:, exclude_player_ids: [])
       round = ::Round.find(round_id)
 
       # TODO: Implement a proper pairing algorithm.
-      round.event.players.shuffle.in_groups_of(2).to_a
+      round.event.players.active.where.not(id: exclude_player_ids).shuffle.in_groups_of(2).to_a
     end
   end
 end
