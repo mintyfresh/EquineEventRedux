1b30ab5b6cc768cb31b181f25445bae3064612bf
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-12 02:18:31 -0400

    Add creation timestamp to secondary order for players.

--- api/app/graphql/types/event_players_order_by_type.rb
+++ api/app/graphql/types/event_players_order_by_type.rb
@@ -6,16 +6,24 @@ module Types
       Player.order(name: direction)
     }
     value 'WINS_COUNT', value: lambda { |direction|
-      Player.joins(:score_card).merge(PlayerScoreCard.order(wins_count: direction)).order(id: direction)
+      Player.joins(:score_card)
+        .merge(PlayerScoreCard.order(wins_count: direction))
+        .order(created_at: direction, id: direction)
     }
     value 'DRAWS_COUNT', value: lambda { |direction|
-      Player.joins(:score_card).merge(PlayerScoreCard.order(draws_count: direction)).order(id: direction)
+      Player.joins(:score_card)
+        .merge(PlayerScoreCard.order(draws_count: direction))
+        .order(created_at: direction, id: direction)
     }
     value 'LOSSES_COUNT', value: lambda { |direction|
-      Player.joins(:score_card).merge(PlayerScoreCard.order(losses_count: direction)).order(id: direction)
+      Player.joins(:score_card)
+        .merge(PlayerScoreCard.order(losses_count: direction))
+        .order(created_at: direction, id: direction)
     }
     value 'SCORE', value: lambda { |direction|
-      Player.joins(:score_card).merge(PlayerScoreCard.order_by_score(direction)).order(id: direction)
+      Player.joins(:score_card)
+        .merge(PlayerScoreCard.order_by_score(direction))
+        .order(created_at: direction, id: direction)
     }
 
     # @return [Proc]
