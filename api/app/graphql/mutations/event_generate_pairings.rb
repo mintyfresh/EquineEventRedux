762d7be4a16a67e9a0e5bbd17610b1725bd9a511
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-11 21:26:50 -0400

    Extract pairing logic into service object

--- api/app/graphql/mutations/event_generate_pairings.rb
+++ api/app/graphql/mutations/event_generate_pairings.rb
@@ -13,43 +13,12 @@ module Mutations
 
     def resolve(event_id:, player_ids:)
       event   = ::Event.find(event_id)
-      players = event.players.active.find(player_ids)
+      players = event.players.active.includes(:score_card).find(player_ids)
 
-      weighted_edges = generated_weighted_edges(players.shuffle)
-      graph = GraphMatching::Graph::WeightedGraph[*weighted_edges]
-
-      pairings = graph.maximum_weighted_matching(true).edges.map do |(player1, player2)|
-        { player1: players[player1], player2: players[player2] }
-      end
+      pairings = PlayerPairingService.new.generate_pairings(players.shuffle)
+      pairings = pairings.map { |(player1, player2)| { player1:, player2: } }
 
       { pairings: }
-    end
-
-  private
-
-    # @param players [Array<::Player>]
-    # @return [Array<(Integer, Integer, Numeric)>]
-    def generated_weighted_edges(players)
-      players = [*players, nil] if players.count.odd?
-
-      players.combination(2).map do |player1, player2|
-        [players.index(player1), players.index(player2), pairing_weight(player1, player2)]
-      end
-    end
-
-    # @param player1 [::Player]
-    # @param player2 [::Player, nil]
-    # @return [Numeric]
-    def pairing_weight(player1, player2)
-      return -9_999_999 if player1.score_card.opponent_ids.include?(player2&.id)
-
-      player1_score = player1 ? player1.score_card.score : 0
-      player2_score = player2 ? player2.score_card.score : 0
-
-      min = [player1_score, player2_score].min
-      max = [player1_score, player2_score].max
-
-      ((max + min) / 2.0) - ((max - min)**2)
     end
   end
 end
---
+++ api/app/services/player_pairing_service.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class PlayerPairingService
+  PREVIOUSLY_PAIRED_PENALTY = -9_999_999
+
+  # @param players [Array<Player>]
+  # @return [Array<(Player, (Player, nil))>]
+  def generate_pairings(players)
+    weighted_edges = generated_weighted_edges(players.shuffle)
+
+    graph = GraphMatching::Graph::WeightedGraph[*weighted_edges]
+    edges = graph.maximum_weighted_matching(true).edges
+
+    edges.map do |(player1, player2)|
+      # Resolve players from the indices
+      # The placeholder player is indexed beyond the end of this array so it becomes nil
+      [players[player1], players[player2]]
+    end
+  end
+
+private
+
+  # @param players [Array<Player>]
+  # @return [Array<(Integer, Integer, Numeric)>]
+  def generated_weighted_edges(players)
+    # Append a placeholder if the number of players is odd
+    players = [*players, nil] if players.count.odd?
+
+    players.combination(2).map do |player1, player2|
+      # Use indices into the array because the graph-matching library expects numeric edges
+      [players.index(player1), players.index(player2), calculate_weight_for_pairing(player1, player2)]
+    end
+  end
+
+  # @param player1 [Player]
+  # @param player2 [Player, nil]
+  # @return [Numeric]
+  def calculate_weight_for_pairing(player1, player2)
+    return PREVIOUSLY_PAIRED_PENALTY if player1.score_card.opponent_ids.include?(player2&.id)
+
+    player1_score = player1 ? player1.score_card.score : 0
+    player2_score = player2 ? player2.score_card.score : 0
+
+    min = [player1_score, player2_score].min
+    max = [player1_score, player2_score].max
+
+    ((max + min) / 2.0) - ((max - min)**2)
+  end
+end
