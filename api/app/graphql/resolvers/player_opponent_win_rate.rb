1d99a4ac7e1d8c19c0ea7fcefda0c8f2c4ca34f8
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-11 22:20:01 -0400

    Extract opponent win-rate calculation to resolver

---
+++ api/app/graphql/resolvers/player_opponent_win_rate.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Resolvers
+  class PlayerOpponentWinRate < BaseResolver
+    type Float, null: false
+
+    # @return [Float]
+    def resolve
+      return 0.0 if total_opponents_count.zero?
+
+      total_opponents_score / total_opponents_count
+    end
+
+  private
+
+    # @return [Float]
+    def total_opponents_score
+      opponent_score_cards.sum { |score_card| score_card.score / score_card.maximum_possible_score.to_f }
+    end
+
+    # @return [Integer]
+    def total_opponents_count
+      score_card.opponent_ids.count
+    end
+
+    # @return [Array<::PlayerScoreCard>]
+    def opponent_score_cards
+      @opponent_score_cards ||= dataloader
+        .with(Sources::Record, ::PlayerScoreCard, :player_id)
+        .load_all(score_card.opponent_ids)
+    end
+
+    # @return [::PlayerScoreCard]
+    def score_card
+      @score_card ||= dataloader.with(Sources::Record, ::PlayerScoreCard, :player_id).load(object.id)
+    end
+  end
+end
--- api/app/graphql/types/player_type.rb
+++ api/app/graphql/types/player_type.rb
@@ -15,7 +15,7 @@ module Types
     field :losses_count, Integer, null: false
     field :score, Integer, null: false
 
-    field :opponent_win_rate, Float, null: false
+    field :opponent_win_rate, resolver: Resolvers::PlayerOpponentWinRate
 
     # @!method wins_count
     #   @return [Integer]
@@ -26,32 +26,6 @@ module Types
     # @!method score
     #   @return [Integer]
     delegate :wins_count, :draws_count, :losses_count, :score, to: :score_card
-
-    # @return [Float]
-    def opponent_win_rate
-      return 0.0 if total_matches_count.zero?
-
-      total_opponents_score / total_matches_count
-    end
-
-  private
-
-    # @return [Float]
-    def total_opponents_score
-      opponent_score_cards.sum { |score_card| score_card.score / score_card.maximum_possible_score.to_f }
-    end
-
-    # @return [Integer]
-    def total_matches_count
-      score_card.completed_matches_count
-    end
-
-    # @return [Array<::PlayerScoreCard>]
-    def opponent_score_cards
-      @opponent_score_cards ||= dataloader
-        .with(Sources::Record, ::PlayerScoreCard, :player_id)
-        .load_all(score_card.opponent_ids)
-    end
 
     # @return [::PlayerScoreCard]
     def score_card
