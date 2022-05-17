d2b7b562ea329e6773635a639439df5b8976827f
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2022-05-17 19:06:24 -0400

    Add players to round object

--- api/app/graphql/types/round_type.rb
+++ api/app/graphql/types/round_type.rb
@@ -5,13 +5,33 @@ module Types
     field :id, ID, null: false
     field :event_id, ID, null: false
     field :number, Integer, null: false
+
     field :matches, [Types::MatchType], null: false
+    field :players, [Types::PlayerType], null: false do
+      extension Extensions::DeletedFilterExtension
+    end
 
     # @return [Array<::Match>]
     def matches
       scope = Match.paired_first.order(:created_at)
 
       dataloader.with(Sources::RecordList, ::Match, :round_id, scope:).load(object.id)
+    end
+
+    # @param deleted [Proc]
+    # @return [Array<::Player>]
+    def players(deleted:)
+      players = Player.all
+      players = deleted.call(players)
+
+      dataloader.with(Sources::Record, ::Player, scope: players).load_all(player_ids).compact
+    end
+
+  private
+
+    # @return [Array<String>]
+    def player_ids
+      @player_ids ||= matches.map(&:player_ids).flatten.compact.uniq
     end
   end
 end
