8e8ea888b859bfaf8b8152200c365d36d4d867fa
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2024-02-17 22:26:59 -0500

    Collapse completed rounds.

--- web/components/Round.tsx
+++ web/components/Round.tsx
@@ -1,10 +1,12 @@
 import { gql } from '@apollo/client'
 import MatchCard, { MATCH_CARD_FRAGMENT } from './MatchCard'
 import { RoundFragment } from '../lib/generated/graphql'
-import React from 'react'
-import { Card, Col, Row } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Card, Col, Collapse, Row } from 'react-bootstrap'
 import RoundControlsDropdown from './RoundList/RoundControlsDropdown'
 import { EDIT_ROUND_DROPDOWN_ITEM_FRAGMENT } from './RoundList/EditRoundDropdownItem'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 export const ROUND_FRAGMENT = gql`
   fragment Round on Round {
@@ -29,36 +31,47 @@ export interface RoundProps extends React.ComponentProps<typeof Card> {
 }
 
 const Round: React.FC<RoundProps> = ({ event, round, onSetWinner, onSetDraw, onDelete, ...props }) => {
+  const [collapsed, setCollapsed] = useState(round.isComplete)
+
   return (
     <>
       <Card {...props}>
         <Card.Header>
           Round {round.number}
+          {round.isComplete && ' (Complete)'}
+          <FontAwesomeIcon
+            icon={collapsed ? faChevronDown : faChevronUp}
+            className="ms-2"
+            role="button"
+            onClick={() => setCollapsed(!collapsed)}
+          />
           <RoundControlsDropdown
             event={event}
             round={round}
             onDelete={() => onDelete?.(round)}
           />
         </Card.Header>
-        <Card.Body className="pb-0">
-          {round.matches.length ? (
-            <Row>
-              {round.matches.map((match) =>
-                <Col key={match.id} lg="3" className="mb-3">
-                  <MatchCard
-                    match={match}
-                    onSetWinner={onSetWinner}
-                    onSetDraw={onSetDraw}
-                  />
-                </Col>
-              )}
-            </Row>
-          ) : (
-            <Card.Text className="mb-3">
-              No pairings have been added to this match yet.
-            </Card.Text>
-          )}
-        </Card.Body>
+        <Collapse in={!collapsed}>
+          <Card.Body className="pb-0">
+            {round.matches.length ? (
+              <Row>
+                {round.matches.map((match) =>
+                  <Col key={match.id} lg="3" className="mb-3">
+                    <MatchCard
+                      match={match}
+                      onSetWinner={onSetWinner}
+                      onSetDraw={onSetDraw}
+                    />
+                  </Col>
+                )}
+              </Row>
+            ) : (
+              <Card.Text className="mb-3">
+                No pairings have been added to this match yet.
+              </Card.Text>
+            )}
+          </Card.Body>
+        </Collapse>
       </Card>
     </>
   )
